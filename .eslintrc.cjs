989f761ed47958d3a7b0b0ecd908ce27c6f3236a
yo <yo@luciditv.com>
2024-03-28 15:24:58 +0800

    set eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,43 +1,34 @@
 /* eslint-env node */
 module.exports = {
-	root: true,
-	'extends': [
-		'plugin:vue/vue3-essential',
-		'eslint:recommended'
-	],
-	parserOptions: {
-		ecmaVersion: 'latest'
-	},
-	rules: {
-		'no-undef': 'off',
-		'semi': ['error', 'never'],
-		'quotes': ['error', 'single'],
-		'indent': ['error', 'tab'],
-		'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
-		'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 1, 'maxBOF': 0 }],
-		'vue/attributes-order': 'error',
-		'vue/html-self-closing': ['error', {
-			'html': {
-				'void': 'always',
-				'normal': 'never',
-				'component': 'always'
-			},
-			'svg': 'always',
-			'math': 'always'
-		}],
-		'vue/html-indent': ['error', 2, {
-			'attribute': 1,
-			'baseIndent': 1,
-			'closeBracket': 0,
-			'alignAttributesVertically': true,
-			'ignores': []
-		}],
-		'vue/max-attributes-per-line': ['error', {
-			'singleline': 3,
-			'multiline': {
-				'max': 1,
-				'allowFirstLine': false
-			}
-		}]
-	},
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'plugin:vue/strongly-recommended',
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-undef': 'off',
+    'semi': ['error', 'never'],
+    'quotes': ['error', 'single'],
+    'indent': ['error', 2, { 'SwitchCase': 1 }],
+    'no-mixed-spaces-and-tabs': 'error',
+    'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 0, 'maxBOF': 0 }],
+    'comma-dangle': ['error', 'always-multiline'],
+    'space-before-function-paren': ['error', 'never'],
+    'space-before-blocks': ['error', 'always'],
+    'array-bracket-spacing': ['error', 'never'],
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { 'before': true, 'after': true }],
+    'key-spacing': ['error', { 'mode': 'strict' }],
+    'no-trailing-spaces': 'error',
+    'no-whitespace-before-property': 'error',
+    'space-in-parens': ['error', 'never'],
+    'space-infix-ops': 'error',
+    'space-unary-ops': ['error', { 'words': true, 'nonwords': false }],
+    'wrap-regex': 'error',
+    'yield-star-spacing': ['error', 'both'],
+  },
 }
